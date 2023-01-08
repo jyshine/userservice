36ee4ed3473fc0eac86e4e8a5a86508ec8572605
june <jyshine3@gmail.com>
2023-01-08 18:18:31 +0900

    feat(init): 초기 세팅 및 DB연결 확인

---
+++ user-service/src/main/java/com/june/sample/userservice/domain/User.java
@@ -0,0 +1,19 @@
+package com.june.sample.userservice.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "users")
+@Setter
+@Getter
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+}
---
+++ user-service/src/main/java/com/june/sample/userservice/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.june.sample.userservice.repository;
+
+import com.june.sample.userservice.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
