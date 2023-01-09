cf58dcbf4b860a16133a53ab6ea6d0e1b09f8412
june <jyshine3@gmail.com>
2023-01-10 03:42:38 +0900

    fix(swagger): UserController tag, operation name 추가

--- user-service/src/main/java/com/june/sample/userservice/user/controller/UserController.java
+++ user-service/src/main/java/com/june/sample/userservice/user/controller/UserController.java
@@ -7,6 +7,8 @@ import com.june.sample.userservice.core.web.RestResponse;
 import com.june.sample.userservice.user.domain.dto.UserRegDTO;
 import com.june.sample.userservice.user.domain.dto.UserSearchDTO;
 import com.june.sample.userservice.user.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,17 +17,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "회원관리")
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
+    @ApiOperation(value = "회원 가입")
     @PostMapping(value = USERS)
     public RestResponse<Boolean> createUser(@Valid @RequestBody UserRegDTO userDTO) {
         return new RestResponse<>(userService.createUser(userDTO));
     }
 
+    @ApiOperation(value = "내 정보 보기")
     @GetMapping(value = USERS_SEARCH)
     public RestResponse<UserSearchDTO> searchUserByUserName(@RequestParam String userName) {
         return new RestResponse<>(userService.searchUserByUserName(userName));
